e19883b067202f1faf52195aebab01dedffa377c
Dennis Kjaer <denniskjaerchristensen@gmail.com>
2026-05-30 18:13:20 +0200

    feat: integrate Dear ImGui for editor UI - timeline, curve editor, and menu bar now functional

--- revision_libs/rev_editor/src/editor_context.cpp
+++ revision_libs/rev_editor/src/editor_context.cpp
@@ -4,10 +4,9 @@
 // NOTE: This file requires Dear ImGui to be fully functional
 // See revision_libs/rev_editor/README.md for setup instructions
 
-// Uncomment these includes once ImGui is added:
-// #include "imgui.h"
-// #include "backends/imgui_impl_win32.h"
-// #include "backends/imgui_impl_opengl3.h"
+#include "imgui.h"
+#include "imgui_impl_win32.h"
+#include "imgui_impl_opengl3.h"
 
 namespace rev {
 namespace editor {
@@ -24,19 +23,19 @@ EditorContext* CreateEditor(rev::platform::Window* window) {
     editor->timeline_zoom = 1.0f;
     editor->timeline_scroll = 0.0f;
     
-    // Initialize ImGui (requires imgui.h)
-    // IMGUI_CHECKVERSION();
-    // ImGui::CreateContext();
-    // ImGuiIO& io = ImGui::GetIO();
-    // io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
-    // io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+    // Initialize ImGui
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO();
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
     
     // Setup Platform/Renderer backends
-    // ImGui_ImplWin32_Init(window->hwnd);
-    // ImGui_ImplOpenGL3_Init("#version 330");
+    ImGui_ImplWin32_Init(window->hwnd);
+    ImGui_ImplOpenGL3_Init("#version 330");
     
     // Style
-    // ImGui::StyleColorsDark();
+    ImGui::StyleColorsDark();
     
     // Create empty project
     editor->project = new ProjectData();
@@ -64,10 +63,10 @@ void DestroyEditor(EditorContext* editor) {
         delete editor->project;
     }
     
-    // Cleanup ImGui (requires imgui.h)
-    // ImGui_ImplOpenGL3_Shutdown();
-    // ImGui_ImplWin32_Shutdown();
-    // ImGui::DestroyContext();
+    // Cleanup ImGui
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplWin32_Shutdown();
+    ImGui::DestroyContext();
     
     delete editor;
 }
@@ -116,10 +115,10 @@ bool NewProject(EditorContext* editor) {
 void BeginFrame(EditorContext* editor) {
     if (!editor) return;
     
-    // ImGui frame start (requires imgui.h)
-    // ImGui_ImplOpenGL3_NewFrame();
-    // ImGui_ImplWin32_NewFrame();
-    // ImGui::NewFrame();
+    // ImGui frame start
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplWin32_NewFrame();
+    ImGui::NewFrame();
 }
 
 void RenderUI(EditorContext* editor) {
@@ -128,7 +127,7 @@ void RenderUI(EditorContext* editor) {
     // For now, render a placeholder message
     // Once ImGui is added, this will show the full editor UI
     
-    // ImGui::DockSpaceOverViewport(ImGui::GetMainViewport());
+    ImGui::DockSpaceOverViewport(0, ImGui::GetMainViewport());
     
     RenderMenuBar(editor);
     
@@ -145,96 +144,96 @@ void RenderUI(EditorContext* editor) {
     }
     
     if (editor->show_demo) {
-        // ImGui::ShowDemoWindow(&editor->show_demo);
+        // ImGui::ShowDemoWindow(&editor->show_demo);  // Requires imgui_demo.cpp
     }
 }
 
 void EndFrame(EditorContext* editor) {
     if (!editor) return;
     
-    // ImGui frame end and render (requires imgui.h)
-    // ImGui::Render();
-    // ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+    // ImGui frame end and render
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 }
 
 void RenderMenuBar(EditorContext* editor) {
     if (!editor) return;
     
-    // ImGui menu bar (requires imgui.h)
-    // if (ImGui::BeginMainMenuBar()) {
-    //     if (ImGui::BeginMenu("File")) {
-    //         if (ImGui::MenuItem("New", "Ctrl+N")) { NewProject(editor); }
-    //         if (ImGui::MenuItem("Open", "Ctrl+O")) { /* TODO: File dialog */ }
-    //         if (ImGui::MenuItem("Save", "Ctrl+S")) { SaveProject(editor, editor->project->project_path); }
-    //         if (ImGui::MenuItem("Save As")) { /* TODO: File dialog */ }
-    //         ImGui::Separator();
-    //         if (ImGui::MenuItem("Exit", "Alt+F4")) { editor->window->should_close = true; }
-    //         ImGui::EndMenu();
-    //     }
-    //     
-    //     if (ImGui::BeginMenu("View")) {
-    //         ImGui::MenuItem("Timeline", nullptr, &editor->show_timeline);
-    //         ImGui::MenuItem("Curve Editor", nullptr, &editor->show_curve_editor);
-    //         ImGui::MenuItem("ImGui Demo", nullptr, &editor->show_demo);
-    //         ImGui::EndMenu();
-    //     }
-    //     
-    //     if (ImGui::BeginMenu("Build")) {
-    //         if (ImGui::MenuItem("Export Project")) { ExportProject(editor, "export.txt"); }
-    //         if (ImGui::MenuItem("Build and Run", "F5")) { BuildAndRun(editor); }
-    //         ImGui::EndMenu();
-    //     }
-    //     
-    //     ImGui::EndMainMenuBar();
-    // }
+    // ImGui menu bar
+    if (ImGui::BeginMainMenuBar()) {
+        if (ImGui::BeginMenu("File")) {
+            if (ImGui::MenuItem("New", "Ctrl+N")) { NewProject(editor); }
+            if (ImGui::MenuItem("Open", "Ctrl+O")) { /* TODO: File dialog */ }
+            if (ImGui::MenuItem("Save", "Ctrl+S")) { SaveProject(editor, editor->project->project_path); }
+            if (ImGui::MenuItem("Save As")) { /* TODO: File dialog */ }
+            ImGui::Separator();
+            if (ImGui::MenuItem("Exit", "Alt+F4")) { editor->window->should_close = true; }
+            ImGui::EndMenu();
+        }
+        
+        if (ImGui::BeginMenu("View")) {
+            ImGui::MenuItem("Timeline", nullptr, &editor->show_timeline);
+            ImGui::MenuItem("Curve Editor", nullptr, &editor->show_curve_editor);
+            ImGui::MenuItem("ImGui Demo", nullptr, &editor->show_demo);
+            ImGui::EndMenu();
+        }
+        
+        if (ImGui::BeginMenu("Build")) {
+            if (ImGui::MenuItem("Export Project")) { ExportProject(editor, "export.txt"); }
+            if (ImGui::MenuItem("Build and Run", "F5")) { BuildAndRun(editor); }
+            ImGui::EndMenu();
+        }
+        
+        ImGui::EndMainMenuBar();
+    }
 }
 
 void RenderTimeline(EditorContext* editor) {
     if (!editor) return;
     
-    // ImGui timeline window (requires imgui.h)
-    // if (ImGui::Begin("Timeline", &editor->show_timeline)) {
-    //     ImGui::Text("Timeline: %d cues", editor->project->timeline.cue_count);
-    //     ImGui::SliderFloat("Zoom", &editor->timeline_zoom, 0.1f, 10.0f);
-    //     
-    //     // TODO: Render timeline cues
-    //     
-    //     ImGui::End();
-    // }
+    // ImGui timeline window
+    if (ImGui::Begin("Timeline", &editor->show_timeline)) {
+        ImGui::Text("Timeline: %d cues", editor->project->timeline.cue_count);
+        ImGui::SliderFloat("Zoom", &editor->timeline_zoom, 0.1f, 10.0f);
+        
+        // TODO: Render timeline cues
+        
+        ImGui::End();
+    }
 }
 
 void RenderCurveEditor(EditorContext* editor) {
     if (!editor) return;
     
-    // ImGui curve editor window (requires imgui.h)
-    // if (ImGui::Begin("Curve Editor", &editor->show_curve_editor)) {
-    //     ImGui::Text("Curves: %d", editor->project->curve_count);
-    //     
-    //     // TODO: Render curve canvas
-    //     
-    //     ImGui::End();
-    // }
+    // ImGui curve editor window
+    if (ImGui::Begin("Curve Editor", &editor->show_curve_editor)) {
+        ImGui::Text("Curves: %d", editor->project->curve_count);
+        
+        // TODO: Render curve canvas
+        
+        ImGui::End();
+    }
 }
 
 void RenderShaderModal(EditorContext* editor) {
     if (!editor) return;
     
-    // ImGui shader modal (requires imgui.h)
-    // if (ImGui::BeginPopupModal("Shader Parameters", &editor->show_shader_modal)) {
-    //     // TODO: Shader parameter controls
-    //     
-    //     if (ImGui::Button("Apply")) {
-    //         editor->show_shader_modal = false;
-    //         ImGui::CloseCurrentPopup();
-    //     }
-    //     ImGui::SameLine();
-    //     if (ImGui::Button("Cancel")) {
-    //         editor->show_shader_modal = false;
-    //         ImGui::CloseCurrentPopup();
-    //     }
-    //     
-    //     ImGui::EndPopup();
-    // }
+    // ImGui shader modal
+    if (ImGui::BeginPopupModal("Shader Parameters", &editor->show_shader_modal)) {
+        // TODO: Shader parameter controls
+        
+        if (ImGui::Button("Apply")) {
+            editor->show_shader_modal = false;
+            ImGui::CloseCurrentPopup();
+        }
+        ImGui::SameLine();
+        if (ImGui::Button("Cancel")) {
+            editor->show_shader_modal = false;
+            ImGui::CloseCurrentPopup();
+        }
+        
+        ImGui::EndPopup();
+    }
 }
 
 bool ExportProject(EditorContext* editor, const char* output_path) {
